c9b34e5d560520dc6763c9cd2bf4c613666e8c96
Jon Ware <jonware01@gmail.com>
2023-07-18 11:27:26 -0400

    Adding sleeping thread and interrupt examples

---
+++ src/com/jware/concurrency/interrupt/InterruptApplication.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.jware.concurrency.interrupt;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jon Ware <jonware01@gmail.com>
+ */
+public class InterruptApplication  {
+    public static void main(String[] args) {
+        /*
+         * Thread interrupt
+         */
+        System.out.println("--------------------------");
+        System.out.println("Starting interrupt example");
+        System.out.println("--------------------------");
+        Thread sleepyThread = new Thread(new SleepyThread());
+        sleepyThread.start();
+        
+        // Interrupt the thread to exit this execution early
+        sleepyThread.interrupt();
+        
+        try {
+            sleepyThread.join();
+        }
+        catch(InterruptedException ex) {
+            Logger.getLogger(InterruptApplication.class.getName()).log(Level.SEVERE, "Join of SleepyThread was interrupted", ex);
+        }
+        
+        /*
+         * Mixed execution
+         */
+        System.out.println("--------------------------------");
+        System.out.println("Starting mixed execution example");
+        System.out.println("--------------------------------");
+        Thread sleepyThread2 = new Thread(new SleepyThread());
+        sleepyThread2.start();
+        
+        Thread sleepyThread3 = new Thread(new SleepyThread());
+        sleepyThread3.start();
+        
+        try {
+            sleepyThread2.join();
+            sleepyThread3.join();
+        }
+        catch(InterruptedException ex) {
+            Logger.getLogger(InterruptApplication.class.getName()).log(Level.SEVERE, "Join of SleepyThread 2 and 3 was interrupted", ex);
+        }
+        
+        /*
+         * Single execution of one thread at a time
+         */
+        System.out.println("-------------------------------------------");
+        System.out.println("Starting single execution at a time example");
+        System.out.println("-------------------------------------------");
+        Thread sleepyThread4 = new Thread(new SleepyThread());
+        sleepyThread4.start();
+        
+        try {
+            sleepyThread4.join();
+        }
+        catch(InterruptedException ex) {
+            Logger.getLogger(InterruptApplication.class.getName()).log(Level.SEVERE, "Join of SleepyThread 4 was interrupted", ex);
+        }
+        
+        Thread sleepyThread5 = new Thread(new SleepyThread());
+        sleepyThread5.start();
+        
+        try {    
+            sleepyThread5.join();
+        }
+        catch(InterruptedException ex) {
+            Logger.getLogger(InterruptApplication.class.getName()).log(Level.SEVERE, "Join of SleepyThread 4 was interrupted", ex);
+        }
+        
+        /*
+         * Delayed thread start
+         */
+        System.out.println("----------------------------------");
+        System.out.println("Starting delayed execution example");
+        System.out.println("----------------------------------");
+        Thread sleepyThread6 = new Thread(new SleepyThread());
+        sleepyThread6.start();
+        
+        try {
+            sleepyThread6.join(1500);
+        }
+        catch(InterruptedException ex) {
+            Logger.getLogger(InterruptApplication.class.getName()).log(Level.SEVERE, "Join of SleepyThread 4 was interrupted", ex);
+        }
+        
+        Thread sleepyThread7 = new Thread(new SleepyThread());
+        sleepyThread7.start();
+        
+        try {    
+            sleepyThread7.join();
+        }
+        catch(InterruptedException ex) {
+            Logger.getLogger(InterruptApplication.class.getName()).log(Level.SEVERE, "Join of SleepyThread 4 was interrupted", ex);
+        }
+    }
+}
---
+++ src/com/jware/concurrency/interrupt/SleepyThread.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.jware.concurrency.interrupt;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jon Ware <jonware01@gmail.com>
+ */
+public class SleepyThread implements Runnable {
+    @Override
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Current thread id: " + Thread.currentThread().threadId() + ", iteration: " + i);
+            
+            try {
+                Thread.sleep(500);
+            } 
+            // This clears the interrupted flag
+            catch (InterruptedException ex) {
+                Logger.getLogger(InterruptApplication.class.getName()).log(Level.SEVERE, "SleepyThread was interrupted", ex);
+                
+                // We are re-setting the interrupted flag to true
+                Thread.currentThread().interrupt();
+                
+                // We must still break as we are not exiting the for loop otherwise
+                // Note: if we don't break the for loop, the for loop will continue
+                // to execute and the exception will be thrown every cycle of the 
+                // loop when the call to sleep checks the interrupted flag.
+                break;
+            }
+        }
+    }
+}
